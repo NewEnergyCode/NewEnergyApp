4b8b51822aa2f2d4b8ad93e3d2740d70c469274a
NewEnergyCode <matveychuk.m@gmail.com>
2023-07-03 19:57:00 +0300

    I added gallery and change color of toolBar

--- app/src/main/java/com/example/newenergyschool/DirectoryInfo.java
+++ app/src/main/java/com/example/newenergyschool/DirectoryInfo.java
@@ -23,33 +23,10 @@ public class DirectoryInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory_inform);
-
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-
-// Получаем ссылку на файл в Firebase Storage
-        StorageReference imageRef = storageRef.child("16185.jpg");
-
-// Получаем URL файла
-
         ImageView directionImage = findViewById(R.id.direction_rev_image);
         ImageView directionLogo = findViewById(R.id.direction_rev_logo);
         TextView directionTitle = findViewById(R.id.direction_rev_title);
         TextView textRev = findViewById(R.id.direction_rev_min_tv);
-        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                // Загружаем изображение в ImageView с помощью библиотеки Picasso
-                Picasso.get().load(uri).into(directionImage);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Обработка ошибки при получении URL или загрузке изображения
-                Toast.makeText(DirectoryInfo.this, "Ошибка загрузки изображения", Toast.LENGTH_SHORT).show();
-            }
-        });
         directionImage.setImageResource(getIntent().getIntExtra("directionImage", 0));
         directionLogo.setImageResource(getIntent().getIntExtra("directionLogo", 0));
         directionTitle.setText(getIntent().getStringExtra("directionTitle"));
